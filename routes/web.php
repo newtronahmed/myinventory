1533c7f6d53fb1ed8c19940ff3edca505e8fa7a0
newtron_ahmed <bazubairu@st.ug.edu.gh>
2021-01-24 14:32:48 +0000

    solved

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::post('/add-brand','BrandsController@store')->name('brands.store');
 Route::get('/products/manage','ProductsController@edit')->name('products.manage');
 Route::get('/products','ProductsController@index');
 Route::get('/products/{id}','ProductsController@show');
-Route::get('/download-pdf/{hash_Id}','ordersController@show')->name('download.show');
+Route::get('/download-pdf/{hash_Id}','OrdersController@show')->name('download.show');
 Route::patch('/products/edit/{product}','ProductsController@update')->name('products.update');
 Route::get('/products/delete/{product}','ProductsController@trash');
 Route::get('/category/manage','CategoriesController@edit')->name('categories.manage');
@@ -36,6 +36,6 @@ Route::get('/brand/manage','BrandsController@edit')->name('brands.manage');
 Route::patch('/brand/edit/{brand}','BrandsController@update');
 Route::get('/category/delete/{category}','CategoriesController@delete');
 Route::get('/brand/delete/{brand}','BrandsController@delete');
-Route::get('/download/pdf/{hash_Id}','ordersController@createPDF');
+Route::get('/download/pdf/{hash_Id}','OrdersController@createPDF');
 Route::post('/neworder','OrdersController@store')->name('order.store');
 
